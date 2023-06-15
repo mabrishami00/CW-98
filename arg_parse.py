d8af659d339c945151a59150f4e06572cb33fc28
mabrishami00 <mabrishami78@yahoo.com>
2023-06-15 16:18:30 +0330

    Add show dir and size functionality

--- arg_parse.py
+++ arg_parse.py
@@ -3,13 +3,36 @@ import os
 
 # Question 1
 
+# parser = argparse.ArgumentParser()
+# parser.add_argument("first_number", type=int)
+# parser.add_argument("second_number", type=int)
+# args = parser.parse_args()
+# summation = args.first_number + args.second_number
+# print(f"summation:{summation}")
+
+# Question 2
+
+# parser = argparse.ArgumentParser()
+# parser.add_argument("path")
+# args = parser.parse_args()
+# path = args.path
+# dirs = os.listdir(path)
+# print(dirs)
+
+# Question 3
+
 parser = argparse.ArgumentParser()
-parser.add_argument("first_number", type=int)
-parser.add_argument("second_number", type=int)
+parser.add_argument("--size", action="store_true")
+parser.add_argument("path_inp")
 args = parser.parse_args()
-summation = args.first_number + args.second_number
-print(f"summation:{summation}")
+path_input = args.path_inp
+dirs = os.listdir(path_input)
 
-#Question 2
-
-
+if args.size is True:
+    for dir in dirs:
+        size = os.path.getsize(dir)
+        print(dir,": ", size)
+else:
+    for dir in dirs:
+        print(dir)
+    
