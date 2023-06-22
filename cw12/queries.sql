64919a76ce467765fa3bbb346fdbc2b62b06ecfd
mabrishami00 <mabrishami78@yahoo.com>
2023-06-22 15:18:08 +0330

    number of films in each category query

--- cw12/queries.sql
+++ cw12/queries.sql
@@ -8,3 +8,8 @@ select title,category.name,release_year from film
 join film_category on film.film_id = film_category.film_id
 join category on film_category.category_id = category.category_id
 where category.name in ('Action','Comedy','Family');
+
+select category.name, count(*) as category_count from film
+join film_category on film.film_id = film_category.film_id
+join category on film_category.category_id = category.category_id
+group by category.name;
