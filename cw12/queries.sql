db1a5ca2204d34d14a993d790b5a5a6e7ba0d3c5
mabrishami00 <mabrishami78@yahoo.com>
2023-06-22 15:39:01 +0330

    show films by category and language query

--- cw12/queries.sql
+++ cw12/queries.sql
@@ -20,3 +20,8 @@ join category on film_category.category_id = category.category_id
 group by category.name
 having count(*) >= 60 and count(*) <= 68;
 
+select title, language.name, category.name from film
+join language on film.language_id = language.language_id
+join film_category on film.film_id = film_category.film_id
+join category on film_category.category_id = category.category_id;
+
