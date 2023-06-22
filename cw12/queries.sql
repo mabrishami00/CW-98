1d1d3855843ef9a8f58328dce2fdac47542228fa
mabrishami00 <mabrishami78@yahoo.com>
2023-06-22 15:17:35 +0330

    films that are comedy or family or action query

--- cw12/queries.sql
+++ cw12/queries.sql
@@ -1,6 +1,10 @@
 
 
-select title,category,release_year from film
+select title,category.name,release_year from film
 join film_category on film.film_id = film_category.film_id
 join category on film_category.category_id = category.category_id;
 
+select title,category.name,release_year from film
+join film_category on film.film_id = film_category.film_id
+join category on film_category.category_id = category.category_id
+where category.name in ('Action','Comedy','Family');
