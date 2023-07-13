ed8fe74a540c2442cc71baad8d420ae92498cd00
mabrishami00 <mabrishami78@yahoo.com>
2023-07-13 15:50:40 +0330

    from starting project until str for post

---
+++ cw15/BlogPlatform/Blogapp/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    publication_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+class Comment(models.Model):
+    post = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    content = models.CharField(max_length=255)
+    date = models.DateField()
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    bio = models.TextField()
