3905240f1a3dc978b84af4d6a5b54f6c92526f90
mabrishami00 <mabrishami78@yahoo.com>
2023-07-14 10:15:47 +0330

    add author_list and author_details

--- cw15/BlogPlatform/Blogapp/views.py
+++ cw15/BlogPlatform/Blogapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Author
 
 
 # Create your views here.
@@ -15,3 +15,12 @@ def post_list(request):
 def post_details(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, "post_details.html", {"post": post})
+
+def author_list(request):
+    authors = Author.objects.all()
+    return render(request, "author_list.html", {"authors": authors})
+
+
+def author_details(request, pk):
+    author = get_object_or_404(Author, pk=pk)
+    return render(request, "author_details.html", {"author": author})
