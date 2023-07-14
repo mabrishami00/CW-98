34282bbbabb5551f3edac58ff580547fd6e37de3
mabrishami00 <mabrishami78@yahoo.com>
2023-07-14 17:20:07 +0330

    final changes

--- cw15/BlogPlatform/Blogapp/models.py
+++ cw15/BlogPlatform/Blogapp/models.py
@@ -18,16 +18,17 @@ class Post(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     publication_date = models.DateField(auto_now_add=True)
+    image = models.ImageField(upload_to="post_images/", null=True, blank=True)
 
     def __str__(self):
         return f"{self.title}"
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     author = models.CharField(max_length=255)
     content = models.CharField(max_length=255)
-    date = models.DateField()
+    date = models.DateField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.author}"
+        return f"{self.author},{self.post.title}"
