7faa36735a5e9c2aaf34af397a6ffbeed4cb28a0
mabrishami00 <mabrishami78@yahoo.com>
2023-07-14 11:54:34 +0330

    add relations

--- cw15/BlogPlatform/Blogapp/models.py
+++ cw15/BlogPlatform/Blogapp/models.py
@@ -1,25 +1,7 @@
 from django.db import models
-
+from Categoryapp.models import Category
 
 # Create your models here.
-class Post(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
-    publication_date = models.DateField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.title}"
-
-
-class Comment(models.Model):
-    post = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
-    content = models.CharField(max_length=255)
-    date = models.DateField()
-
-    def __str__(self):
-        return f"{self.author}"
 
 
 class Author(models.Model):
@@ -28,3 +10,24 @@ class Author(models.Model):
 
     def __str__(self):
         return f"{self.name}"
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    publication_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.CharField(max_length=255)
+    content = models.CharField(max_length=255)
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.author}"
--- cw15/BlogPlatform/Categoryapp/models.py
+++ cw15/BlogPlatform/Categoryapp/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 # Create your models here.
 class Category(models.Model):
     label = models.CharField(max_length=255)
     description = models.TextField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
+
 
     def __str__(self):
         return f"{self.label}"
